79af9e821bcf948cd68531a86bed1fcf030e8c07
Blessing <tioluwani001@gmail.com>
2025-05-19 15:11:14 -0600

    add changes to package.json file

--- app.js
+++ app.js
@@ -42,6 +42,8 @@ var posts = [
 			"Lorem ipsum odor amet, consectetuer adipiscing elit. Curabitur nullam egestas nibh senectus himenaeos mollis arcu. Nisi accumsan taciti rhoncus elementum per nisl sed. Egestas sollicitudin per pretium sagittis dapibus ultrices arcu taciti interdum. Ac lobortis montes ultricies interdum maecenas etiam sem. Facilisi cras lacinia mi erat torquent non. Consectetur mi urna nibh dolor elit. Etiam mus platea phasellus accumsan amet. Sociosqu tellus euismod condimentum porttitor justo volutpat. Efficitur libero duis, amet semper proin pharetra arcu nisi nec. Tristique lectus fusce habitant platea vulputate eu. Faucibus viverra netus aptent felis augue. Fringilla commodo nulla libero aptent finibus turpis nisl bibendum. Placerat inceptos convallis dui felis tortor felis potenti. Lobortis finibus convallis scelerisque viverra vulputate ipsum hac. Porttitor imperdiet porta nunc rhoncus risus sed a urna. Primis suscipit ligula ligula orci cursus, felis habitasse. Odio porta vel tincidunt curae fames. Gravida per et accumsan, pharetra elementum adipiscing varius etiam. Hendrerit mollis dapibus amet luctus ipsum ante. Quam sociosqu himenaeos pharetra eleifend duis penatibus tellus. Aenean taciti lectus congue mus facilisi hendrerit! Etiam leo dignissim maximus felis eget porttitor cras. Euismod massa nisl blandit; fusce maximus dis.",
 	},
 ];
+
+// console.log(posts);
 // Set EJS as the view engine
 app.set("view engine", "ejs");
 
@@ -56,6 +58,23 @@ app.get("/create", (req, res) => {
 	res.render("create.ejs");
 });
 
+app.get("/update", (req, res) => {
+	let id = req.query.id;
+	id = parseFloat(id);
+	console.log(id);
+
+	let post = posts.find((post) => post.postId == id);
+	console.log("All posts:", posts);
+
+	console.log(`Post: ${JSON.stringify(post)}`);
+	res.render("update.ejs", {
+		id: id,
+		title: posts[id - 1].postTitle,
+		description: posts[id - 1].postDescription,
+		content: posts[id - 1].postBody,
+	});
+});
+
 // create and display post
 app.post("/submit", (req, res) => {
 	let title = req.body.postTitle;
@@ -68,12 +87,31 @@ app.post("/submit", (req, res) => {
 		postDescription: description,
 		postBody: content,
 	});
-	console.log(title, description, content);
+	// console.log(title, description, content);
 	res.render("post.ejs", {
 		title: title,
 		description: description,
 		content: content,
 	});
+	//route them to the view page
+});
+app.post("/submit2", (req, res) => {
+	let title = req.body.postTitle;
+	let description = req.body.postDescription;
+	let content = req.body.postContent;
+	let id = req.body.id;
+
+	// find the original post
+	let post = posts.find((post) => post.postId == id);
+
+	//include the update
+
+	(post.postId = id),
+		(post.postTitle = title),
+		(post.postDescription = description),
+		(post.postBody = content),
+		// console.log(title, description, content);
+		res.render("blog.ejs", { posts: posts });
 	//route them to the view page
 });
 
